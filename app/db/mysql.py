6edc6432b2ff9eb94ab4bfea1dd11c3c07c4db69
user <3632438953@qq.com>
2025-12-25 12:43:35 +0800

    1

---
+++ app/db/mysql.py
@@ -0,0 +1,74 @@
+# pymysql库进行连接和CRUD
+
+import os
+import pymysql
+from contextlib import contextmanager
+
+MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
+MYSQL_USER = os.getenv("MYSQL_USER", "tom")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "123456")
+MYSQL_DB = os.getenv("MYSQL_DB", "enterprise_kb")
+
+@contextmanager
+def get_conn():  # 获取数据库连接
+    conn = pymysql.connect(
+        host=MYSQL_HOST, port=MYSQL_PORT,
+        user=MYSQL_USER, password=MYSQL_PASSWORD,
+        database=MYSQL_DB, charset="utf8mb4",
+        autocommit=True,
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+def get_leave_balance(requester: str) -> dict | None:
+    # 根据用户名requester查询这个用户还有几天年假，病假。。。可以请
+    with get_conn() as conn:  # 获得连接
+        with conn.cursor() as cur:  # 获得游标
+            cur.execute(
+                "SELECT annual_days, sick_days, personal_days FROM leave_balances WHERE requester=%s",
+                (requester,)
+            ) # 执行SQL语句
+            return cur.fetchone()  # 获得返回结果
+
+
+def insert_leave_request(req: dict) -> str:
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO leave_requests
+                (leave_id, requester, leave_type, start_time, end_time, duration_days, reason, status)
+                VALUES (%s,%s,%s,%s,%s,%s,%s,'PENDING')
+                """,
+                (
+                    req["leave_id"], req["requester"], req["leave_type"],
+                    req["start_time"], req["end_time"], req["duration_days"],
+                    req.get("reason")
+                )
+            )
+    return req["leave_id"]
+
+
+def get_leave_request(leave_id: str) -> dict | None:
+    with get_conn() as conn:  #
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM leave_requests WHERE leave_id=%s", (leave_id,))
+            return cur.fetchone()
+
+
+def cancel_leave_request(leave_id: str) -> bool:
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "UPDATE leave_requests SET status='CANCELLED' WHERE leave_id=%s AND status='PENDING'",
+                (leave_id,)
+            )
+            return cur.rowcount > 0
+
+
+print(get_leave_balance("peter"))
